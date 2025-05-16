5b032a6d7bc8e885d6fe42764c19ad08be8f59b8
Nader-Kojok <naderkojok@gmail.com>
2025-05-16 19:12:48 +0000

    sharing card added.

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,7 +52,7 @@ export const metadata: Metadata = {
     description: 'Transformez vos idées en applications web et mobiles performantes avec ArmaSoft. Expertise en développement et solutions sur mesure.',
     images: [
       {
-        url: 'https://armasoft.com/images/og-image.jpg',
+        url: 'https://armasoft.com/card_ok.png',
         width: 1200,
         height: 630,
         alt: 'ArmaSoft - Solutions Digitales Innovantes'
@@ -61,9 +61,9 @@ export const metadata: Metadata = {
   },
   twitter: {
     card: 'summary_large_image',
-    title: 'ArmaSoft - Solutions Digitales Innovantes au Sénégal',
+    title: 'ArmaSoft - Solutions Digitales Innovantes en Côte d\'Ivoire',
     description: 'Développement web et mobile, solutions sur mesure. Transformez vos idées en applications performantes avec ArmaSoft.',
-    images: ['https://armasoft.com/images/twitter-image.jpg'],
+    images: ['https://armasoft.com/card_ok.png'],
     creator: '@ArmaSoft',
     site: '@ArmaSoft'
   },
